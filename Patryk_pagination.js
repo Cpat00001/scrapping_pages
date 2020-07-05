644f51e802e6cb8700edaf1cddb0d28972ab468a
Pat <p171982@gmail.com>
2020-07-05 19:07:54 +0100

    some new

--- Patryk_pagination.js
+++ Patryk_pagination.js
@@ -33,7 +33,7 @@
     //let endPath = document.getElementById('more').getAttribute("href");
     let endPath = document.getElementById('more').getAttribute("value");
     //let test = 'https://olivias-dev.myshopify.com/collections/lamps/products.json';
-    let test = '/admin/api/2020-04/products.json?limit=6&collection_id=157079961644';
+    let test = '/admin/api/2020-04/products.json?limit=10&collection_id=157079961644';
     console.log( "SHOW full path:" + nextUrl);
     console.log( "SHOW end of path:" + endPath);
     //count button clicks
@@ -50,42 +50,70 @@
                 .done(function(data){
                     //stringify JSON
                     let productsList = JSON.stringify(data);
-                    console.log("SHOW DATA TYPE: " + typeof productsList + " DATA " + productsList );
+                    //console.log("SHOW DATA TYPE: " + typeof productsList + " DATA " + productsList );
                     let products = data.products;
+
                     //get amount/length of array elements
                     console.log("products.length: " + products.length);
 
-                    var box = document.getElementById('elem');
-                     //create row
-                     var row = document.createElement('div');
-                        row.className = "grid grid--uniform grid--collection small--grid--flush";
+                    //get info about amount of products in array
+                    let ids = products.map((item) => item.id);
+                    console.log('List of IDS(whole set) ' + ids + '</br>');
+                    let id = '4385385578540'; //I need to know the id number of last index of element loaded to page on URL 
+                    for(var i = 0; i < products.length;i++){
+                        if(products[i].id == id){
+                            let firstResult = i;
+                            var firstPlusOne = firstResult + 1;
+                            console.log("Numer indexu do ID: " + i + " dodany numer do indexu: " + firstPlusOne);
+                        };
+                    };
+                    // get required N number of elements from the last displayed on page orignailly on URL 
+                    function getFirstNNResults(firstPlusOne){
+                                var firstRequiredElements = products.slice(firstPlusOne,6);
+                                //humanreadable arguments to console
+                                let firtRequiredIDS = firstRequiredElements.map((item) => item.id);
+                                console.log("IDs of FirstReuired/Selected from whole set: " + firtRequiredIDS );
+                                //console.log("First2: " + JSON.stringify(firstRequiredElements) );
+                                alert('Get Required elements first');
+                                // return firstRequiredElements;
 
-                    products.forEach(function(product){
-                        
-                        //column element with <img>
-                        var div = document.createElement('div');
-                        div.className = "grid__item grid-product small--one-half medium-up--one-third aos-init aos-animate";
-                        //create link
-                        var link = document.createElement('a');
-                        link.href = 'https://olivias-dev.myshopify.com/collections/lamps/products/' + product.handle;
-                        var tytul = document.createElement('p');
-                        tytul.innerHTML = product.title;
-                        tytul.className = "grid-product__title grid-product__title--body";
-                        var img = document.createElement('img');
-                        img.src = product.images[0].src;
-                        var price = document.createElement('p');
-                        price.innerHTML = '£' + product.variants[0].price;
-                        price.className="grid-product__price";
-                        
-                        link.appendChild(img);
-                        link.appendChild(tytul);
-                        link.appendChild(price);
-                        div.appendChild(link);
-                        row.appendChild(div);
-                        box.appendChild(row);
- 
-                    });
-                    // row.appendChild(div);
+                                // build grid and display firsSelectedElements
+                                
+                                var box = document.getElementById('elem');
+                                        //create row
+                                        var row = document.createElement('div');
+                                            row.className = "grid grid--uniform grid--collection small--grid--flush";
+
+                                            firstRequiredElements.forEach(function(product){
+                                            
+                                            //column element with <img>
+                                            var div = document.createElement('div');
+                                            div.className = "grid__item grid-product small--one-half medium-up--one-third aos-init aos-animate";
+                                            //create link
+                                            var link = document.createElement('a');
+                                            link.href = 'https://olivias-dev.myshopify.com/collections/lamps/products/' + product.handle;
+                                            var tytul = document.createElement('p');
+                                            tytul.innerHTML = product.title;
+                                            tytul.className = "grid-product__title grid-product__title--body";
+                                            var img = document.createElement('img');
+                                            img.src = product.images[0].src;
+                                            var price = document.createElement('p');
+                                            price.innerHTML = '£' + product.variants[0].price;
+                                            price.className="grid-product__price";
+                                            
+                                            link.appendChild(img);
+                                            link.appendChild(tytul);
+                                            link.appendChild(price);
+                                            div.appendChild(link);
+                                            row.appendChild(div);
+                                            box.appendChild(row);
+                    
+                                        });
+
+                            }
+                        getFirstNNResults();
+
+                    console.log("Amount of products:" + productsList + " ids: " + ids);
                  
                 });
         console.log('CLICKED');
