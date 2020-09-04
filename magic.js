3d18248e00ec682e729cafbf067c851c29e8287b
Pat <p171982@gmail.com>
2020-09-04 21:18:05 +0100

    some new

---
+++ magic.js
@@ -0,0 +1,8 @@
+function coverDiv(color) {
+  const x = document.getElementById('czwartyBox');
+  x.style.backgroundColor = color;
+}
+function normalDiv(color) {
+  const x = document.getElementById('czwartyBox');
+  x.style.backgroundColor = color;
+}
