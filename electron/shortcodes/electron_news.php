4f7931fb066035f7c2ff956767d362e60afcf8dc
Pat <p171982@gmail.com>
2020-10-18 12:22:32 +0100

    some new wordpress

--- electron/shortcodes/electron_news.php
+++ electron/shortcodes/electron_news.php
@@ -2,45 +2,33 @@
 
 function practice_areas(){
     $query = new WP_Query(
-        array(
-            'post_type' => 'practice_areas',
-            'post_status' => 'publish',
-            'post_per_page' => -1,
-            'order' => 'ASC',
-            'orderby' => 'menu_order'
+        array('post_type' => 'practice-areas',
+              'post_status' => 'publish',
+              'post_per_page' => -1,
+              'order'=> 'ASC',
+              'orderby' => 'menu_order'
         )
     );
     $i = 1;
     $str = '<div class="elementor-row">';
     while($query->have_posts()): 
         $query->the_post();
-        // $str .= '
-        // <div class="elementor-element elementor-view-framed elementor-shape-square elementor-position-top elementor-vertical-align-top elementor-widget elementor-widget-icon-box animated fadeInDown" data-element_type="widget">
-        //     <div class="practice-area-box homepage-services">
-        //         <div class="content">
-        //             <a class="icon" href="'.get_the_permalink().'">
-        //             <i aria-hidden="true" class="'.do_shortcode('[act field="icon"]').'"></i></a>
-        //             <h3 class="title">'.get_the_title().'</h3>
-        //             <p class="description">'.do_shortcode('[act field="blurb"]').'</p>
-        //         </div>            
-        //     </div>
-        // </div>
-        // ';
         $str .= '
-        <div>
-            <div>
-                <div>
+        <div class="elementor-column elementor-col-33 elementor-top-column elementor-element" data-element-type="column">
+            <div class="practice-area-box homepage-services">
+                <div class="content">
                     <a class="icon" href="'.get_the_permalink().'">
-                    <i aria-hidden="true" class="'.do_shortcode('[act field="icon"]').'"></i></a>
+                    <i aria-hidden="true" class="'.do_shortcode('[acf field="icon"]').'"></i>
+                    </a>
                     <h3 class="title">'.get_the_title().'</h3>
-                    <p class="description">'.do_shortcode('[act field="blurb"]').'</p>
+                    <p class="description">'.do_shortcode('[acf field="blurb"]').'</p>
                 </div>            
             </div>
         </div>
         ';
         if($i % 3 == 0):
             $str .= '</div>'; 
-            $str .='<div class="elementor-row">'; 
+            $str .='<div class="elementor-row">';
         endif;
         $i++;
     endwhile;
@@ -68,3 +56,34 @@ function electron_news_link(){
 }
 
 add_shortcode('electron_news_link', 'electron_news_link');
+
+
+
+// wordpress functionality to create columns in CMS
+add_filter('manage_practice-areas_posts_columns','practice_areas_columns');
+
+function practice_areas_columns($columns){
+    $columns = array(
+        'cb' => 'cb',
+        'title' => 'Title',
+        'order' => 'Order',
+        'date' => 'date'
+    );
+    return $columns;
+}
+
+add_filter ('manage_edit-practice-areas_sortable_columns','practice_areas_sortable_columns');
+
+function practice_areas_sortable_columns($columns){
+    $columns['order'] = 'order';
+    return $columns;
+}
+add_action('manage_practice-areas_posts_custom_column','practice_areas_show_columns');
+
+function practice_areas_show_columns($column_name){
+    global $post;
+    if($column_name == 'order'):
+        echo $post->menu_order;
+    endif;
+}
+
