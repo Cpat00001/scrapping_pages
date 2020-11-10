90d327e34a14d61b8c2976dd31027dfa5b6c52bc
Pat <p171982@gmail.com>
2020-11-10 22:01:14 +0000

    add javascript file

--- woo_comm_three/functions.php
+++ woo_comm_three/functions.php
@@ -6,4 +6,11 @@ function scandinavian_stylesheet(){
 }
 add_action('wp_enqueue_scripts','scandinavian_stylesheet');
 
+// add javascript
+function add_javascript(){
+    wp_register_script('custom_javascript',get_template_directory_uri(). '/app.js', array('jquery'),'1',true);
+    wp_enqueue_script('custom_javascript');
+}
+add_action('wp_enqueue_scripts','add_javascript');
 ?>
+
